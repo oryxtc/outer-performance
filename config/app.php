c500d630760170177bb038da0ac965ceeaa8d602
oryxtc <oryxtc@gmail.com>
2017-06-23 13:52:36 +0800

    增加voyager 服务者

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Package Service Providers
+        TCG\Voyager\VoyagerServiceProvider::class,
+
     ],
 
     /*
