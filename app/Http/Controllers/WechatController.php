570b72097f94fdb20767a878a6fedf6fc65afb88
oryxtc <oryxtc@gmail.com>
2017-06-23 18:05:56 +0800

    修改返回内容

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -17,7 +17,7 @@ class WechatController extends Controller
 
         $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 overtrue！";
+            return "欢迎关注 overtrue!！";
         });
 
         \Log::info('return response.');
