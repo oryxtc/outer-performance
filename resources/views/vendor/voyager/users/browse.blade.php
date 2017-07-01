6ed8a9777a98be5d2700f1e190e0f31c0c8dc76a
oryxtc <oryxtc@gmail.com>
2017-07-01 13:34:48 +0800

    完成按钮修改

--- resources/views/vendor/voyager/users/browse.blade.php
+++ resources/views/vendor/voyager/users/browse.blade.php
@@ -282,10 +282,19 @@
         });
 
         //删除按钮
-        $('td').on('click', '.delete', function (e) {
-            console.log(11)
+        var deleteFormAction;
+        $('table').on('click', '.delete', function (e) {
             var form = $('#delete_form')[0];
-            form.action = parseActionUrl(form.action, $(this).data('id'));
+
+            if (!deleteFormAction) { // Save form action initial value
+                deleteFormAction = form.action;
+            }
+
+            form.action = deleteFormAction.match(/\/[0-9]+$/)
+                    ? deleteFormAction.replace(/([0-9]+$)/, $(this).data('id'))
+                    : deleteFormAction + '/' + $(this).data('id');
+            console.log(form.action);
+
             $('#delete_modal').modal('show');
         });
     </script>
