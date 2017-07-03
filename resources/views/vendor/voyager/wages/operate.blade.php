c41e3d4ef08ef00e6827d950745cf015331247de
oryxtc <oryxtc@gmail.com>
2017-07-03 17:21:53 +0800

    新增 工资

---
+++ resources/views/vendor/voyager/wages/operate.blade.php
@@ -0,0 +1,19 @@
+@if (Voyager::can('delete_provindets'))
+    <a href="javascript:;" title="Delete" class="btn btn-sm btn-danger pull-right delete" data-id="{{ $provident->id }}"
+       id="delete-{{ $provident->id }}">
+        <i class="voyager-trash"></i> <span class="hidden-xs hidden-sm">删除</span>
+    </a>
+@endif
+@if (Voyager::can('edit_provindets'))
+    <a href="{{ route('voyager.providents.edit', $provident->id) }}" title="Edit"
+       class="btn btn-sm btn-primary pull-right edit">
+        <i class="voyager-edit"></i> <span class="hidden-xs hidden-sm">修改</span>
+    </a>
+@endif
+@if (Voyager::can('read_providents'))
+    <a href="{{ route('voyager.providents.show', $provident->id) }}" title="View"
+       class="btn btn-sm btn-warning pull-right">
+        <i class="voyager-eye"></i> <span class="hidden-xs hidden-sm">查看</span>
+    </a>
+@endif
+
