709971d72f15d2f46a2e2f1dabda92773573a3e9
oryxtc <oryxtc@gmail.com>
2017-07-05 15:26:39 +0800

    修复

--- app/Http/Controllers/Voyager/VoyagerUserController.php
+++ app/Http/Controllers/Voyager/VoyagerUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Voyager;
 use App\Http\Controllers\ExcelController;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use TCG\Voyager\Facades\Voyager;
 use TCG\Voyager\Models\Role;
 
@@ -20,7 +21,7 @@ class VoyagerUserController extends VoyagerBreadController
         $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
         // Check permission
-        Voyager::canOrFail('browse_'.$dataType->name);
+        Voyager::canOrFail('browse_' . $dataType->name);
 
         $getter = $dataType->server_side ? 'paginate' : 'get';
 
@@ -56,8 +57,8 @@ class VoyagerUserController extends VoyagerBreadController
             $view = "voyager::$slug.browse";
         }
         //多选框字段
-        $checkData=ExcelController::CHECK_DATA;
-        return view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable','checkData'));
+        $checkData = ExcelController::CHECK_DATA;
+        return view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable', 'checkData'));
     }
 
 
@@ -68,7 +69,7 @@ class VoyagerUserController extends VoyagerBreadController
         $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
         // Check permission
-        Voyager::canOrFail('edit_'.$dataType->name);
+        Voyager::canOrFail('edit_' . $dataType->name);
 
         $relationships = $this->getRelationships($dataType);
 
@@ -92,14 +93,14 @@ class VoyagerUserController extends VoyagerBreadController
     public function update(Request $request, $id)
     {
         //验证数据
-        $this->validatorEdit($request->all())->validate();
+        $this->validatorEdit($request->all(),$id)->validate();
 
         $slug = $this->getSlug($request);
 
         $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
         // Check permission
-        Voyager::canOrFail('edit_'.$dataType->name);
+        Voyager::canOrFail('edit_' . $dataType->name);
 
         //Validate fields with ajax
         $val = $this->validateBread($request->all(), $dataType->editRows);
@@ -116,7 +117,7 @@ class VoyagerUserController extends VoyagerBreadController
             return redirect()
                 ->route("voyager.{$dataType->slug}.edit", ['id' => $id])
                 ->with([
-                    'message'    => "Successfully Updated {$dataType->display_name_singular}",
+                    'message' => "Successfully Updated {$dataType->display_name_singular}",
                     'alert-type' => 'success',
                 ]);
         }
@@ -130,7 +131,7 @@ class VoyagerUserController extends VoyagerBreadController
         $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
         // Check permission
-        Voyager::canOrFail('add_'.$dataType->name);
+        Voyager::canOrFail('add_' . $dataType->name);
 
         $dataTypeContent = (strlen($dataType->model_name) != 0)
             ? new $dataType->model_name()
@@ -160,7 +161,7 @@ class VoyagerUserController extends VoyagerBreadController
         $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
         // Check permission
-        Voyager::canOrFail('add_'.$dataType->name);
+        Voyager::canOrFail('add_' . $dataType->name);
 
         //Validate fields with ajax
         $val = $this->validateBread($request->all(), $dataType->addRows);
@@ -175,7 +176,7 @@ class VoyagerUserController extends VoyagerBreadController
             return redirect()
                 ->route("voyager.{$dataType->slug}.edit", ['id' => $data->id])
                 ->with([
-                    'message'    => "Successfully Added New {$dataType->display_name_singular}",
+                    'message' => "Successfully Added New {$dataType->display_name_singular}",
                     'alert-type' => 'success',
                 ]);
         }
@@ -187,19 +188,19 @@ class VoyagerUserController extends VoyagerBreadController
      * @param $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function resetPass(Request $request,$id)
+    public function resetPass(Request $request, $id)
     {
         //验证数据
-        $validate=$this->validatorPass($request->all());
+        $validate = $this->validatorPass($request->all());
 
-        if($validate->fails()){
-            return $this->apiJson(false,$validate->errors()->first('password'));
+        if ($validate->fails()) {
+            return $this->apiJson(false, $validate->errors()->first('password'));
         }
 
         $dataType = Voyager::model('DataType')->where('slug', '=', 'users')->first();
 
         // Check permission
-        Voyager::canOrFail('edit_'.$dataType->name);
+        Voyager::canOrFail('edit_' . $dataType->name);
 
         //密码加密
         $password = bcrypt($request->get('password'));
@@ -210,9 +211,9 @@ class VoyagerUserController extends VoyagerBreadController
             ->update(['password' => $password]);
 
         if ($update_result === false) {
-            return $this->apiJson(false,'重置密码失败!');
+            return $this->apiJson(false, '重置密码失败!');
         }
-        return $this->apiJson(true,'重置密码成功!');
+        return $this->apiJson(true, '重置密码成功!');
     }
 
 
@@ -221,41 +222,42 @@ class VoyagerUserController extends VoyagerBreadController
      * @param Request $request
      * @return mixed
      */
-    public function getUsersList(Request $request){
+    public function getUsersList(Request $request)
+    {
         $head_list = ExcelController::HEAD_LIST;
-        if(empty($request->get('checkData',[]))){
-            $check_data=ExcelController::CHECK_DATA;
-        }else{
-            $request_check_data=$request->get('checkData',[]);
-            foreach ($request_check_data as $key=>$value){
-                if ($value==='true'){
-                    $check_data[]=$key;
+        if (empty($request->get('checkData', []))) {
+            $check_data = ExcelController::CHECK_DATA;
+        } else {
+            $request_check_data = $request->get('checkData', []);
+            foreach ($request_check_data as $key => $value) {
+                if ($value === 'true') {
+                    $check_data[] = $key;
                 }
             }
         }
-        $users = User::select(array_merge($check_data,['id']))->orderBy('id','DESC');
-        $response_data=\Datatables::eloquent($users);
+        $users = User::select(array_merge($check_data, ['id']))->orderBy('id', 'DESC');
+        $response_data = \Datatables::eloquent($users);
 
         //过滤字段
-        $response_data=$response_data->editColumn('role_id', function(User $user) {
-            return empty($user->role_id)?'未设置角色':$user->getRole->display_name;
+        $response_data = $response_data->editColumn('role_id', function (User $user) {
+            return empty($user->role_id) ? '未设置角色' : $user->getRole->display_name;
         });
         //添加编辑
-        $response_data=$response_data->addColumn('action', function (User $user){
-           return view('voyager::users.operate',['user'=>$user]);
+        $response_data = $response_data->addColumn('action', function (User $user) {
+            return view('voyager::users.operate', ['user' => $user]);
         });
         //指定搜索栏模糊匹配
-        $response_data=$response_data->filter(function ($query) use ($request,$head_list) {
-                foreach ($head_list as $key=>$value){
-                    if ($request->has($key)) {
-                        $query->where($key, 'like', "%{$request->get($key)}%");
-                    }
+        $response_data = $response_data->filter(function ($query) use ($request, $head_list) {
+            foreach ($head_list as $key => $value) {
+                if ($request->has($key)) {
+                    $query->where($key, 'like', "%{$request->get($key)}%");
                 }
-            });
+            }
+        });
         //删除id
-        $response_data=$response_data->removeColumn('id');
+        $response_data = $response_data->removeColumn('id');
         //生成实例
-        $response_data=$response_data->make();
+        $response_data = $response_data->make();
         return $response_data;
     }
 
@@ -265,19 +267,20 @@ class VoyagerUserController extends VoyagerBreadController
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function getUsersNameList(Request $request){
-        $users_list=User::select(['job_number','username','part_name']);
-        if($request->has('username')){
-            $users_list=$users_list->where('username','like',"%{$request->get('username')}%");
+    public function getUsersNameList(Request $request)
+    {
+        $users_list = User::select(['job_number', 'username', 'part_name']);
+        if ($request->has('username')) {
+            $users_list = $users_list->where('username', 'like', "%{$request->get('username')}%");
         }
-        $users_list=$users_list->get()->toArray();
-        return $this->apiJson(true,'',$users_list);
+        $users_list = $users_list->get()->toArray();
+        return $this->apiJson(true, '', $users_list);
     }
 
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -293,14 +296,22 @@ class VoyagerUserController extends VoyagerBreadController
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validatorEdit(array $data)
+    protected function validatorEdit(array $data,$id)
     {
         return \Validator::make($data, [
-            'email' => 'required|string|max:150|unique:users',
-            'job_number' => 'required|string|max:255|unique:users',
+            'email' => [
+                'required',
+                'max:150',
+                Rule::unique('users')->ignore($id),
+            ],
+            'job_number' => [
+                'required',
+                'max:150',
+                Rule::unique('users')->ignore($id),
+            ],
         ]);
     }
 
