0bdf2b6eab2b95b024bc7e33684cc5e35b54a78d
amanda_zs <amanda_zs@outlook.com>
2017-07-07 00:03:07 +0800

    优化

--- last/my-project/src/components/info.vue
+++ last/my-project/src/components/info.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="container container-detail" style="height: 100%">
       <div class="info-panel">
-        <p class="info_title">张三xxx</p>
-        <p class="mt10"><span style="color: #999;margin-right: 12px;">2018-04-19</span>{{user}}</p>
+        <p class="info_title">{{userData.title}}</p>
+        <p class="mt10"><span style="color: #999;margin-right: 12px;">{{userData.created_at}}</span>{{userData.username}}</p>
         <ul class="mt10">
           <li>
-            <span class="left_word">申请时长</span><span>{{reply_time}}</span>
+            <span class="left_word">申请时长</span><span>{{userData.continued_at}}</span>
           </li>
           <li>
-            <span class="left_word">申请类型</span><span>{{reply_type}}</span>
+            <span class="left_word">申请类型</span><span>{{userData.type}}</span>
           </li>
           <li>
-            <span class="left_word">开始时间</span><span>{{begin_time}}</span>
+            <span class="left_word">开始时间</span><span>{{userData.start_at}}</span>
           </li>
           <li>
-            <span class="left_word">结束时间</span><span>{{over_time}}</span>
+            <span class="left_word">结束时间</span><span>{{userData.end_at}}</span>
           </li>
         </ul>
-        <p class="reason mt10">我因【{{reason}}】于 【{{minuteListValue}}】 至 【{{minuteListValue2}}】请假,类型为{{type}}请审批 ! </p>
-        <flexbox class="mt10">
+        <p class="reason mt10">{{userData.reson}}</p>
+        <flexbox class="mt10" v-if="userData.can_review">
           <flexbox-item style="margin-right: 10px;">
               <x-button type="default">退审</x-button>
           </flexbox-item>
@@ -29,40 +29,37 @@
       </div>
       <div class="pass-panel">
         <div class="pass-title">
-          <span>审批人({{passTotalNo}})</span>
+          <span>审批人({{approver.length}})</span>
         </div>
-        <div v-if="passTotalNo > 0">
-          <!--<cell>-->
-            <div id="select-passman" class="clearfix">
-              <div class="options_man"><span></span><img class="man" src="../../src/assets/user-plus.png" alt=""></div>
-              <div class="options_man"><span></span><img class="man" src="../../src/assets/user-minus.png" alt=""></div>
-            </div>
-          <!--</cell>-->
-        </div>
-      </div>
-      <div class="pass-panel">
-        <div class="pass-title" style="border-top: 1px solid #dddddd">
-          <span>相关人({{relateTotalNo}})</span>
-        </div>
-        <div v-if="relateTotalNo > 0">
-          <!--<cell>-->
-            <div id="select-relateman" class="clearfix">
-              <div class="options_man"><span></span><img class="man" src="../../src/assets/user-plus.png" alt=""></div>
-              <div class="options_man"><span></span><img class="man" src="../../src/assets/user-minus.png" alt=""></div>
-            </div>
-          <!--</cell>-->
+        <div v-if="approver.length > 0" >
+          <template v-for="item in approver">
+              <div id="select-passman" class="clearfix">
+                <div class="options_man"><span>{{item}}</span><img class="man" src="../../src/assets/user-plus.png" alt=""></div>
+              </div>
+          </template>
         </div>
       </div>
       <div class="pass-panel">
         <div class="pass-title" style="border-top: 1px solid #dddddd">
-          <span>回复({{answerTotalNo}})</span>
+          <span>相关人({{relevant.length}})</span>
         </div>
-        <div v-if="answerTotalNo > 0">
-            <template v-for="item in answerList">
+        <div v-if="relevant.length > 0" >
+          <template v-for="item in relevant">
+              <div id="select-relateman" class="clearfix">
+                <div class="options_man"><span>{{item}}</span><img class="man" src="../../src/assets/user-plus.png" alt=""></div>
+              </div>
+          </template>
+        </div>
+      </div>
+      <div class="pass-panel">
+        <div class="pass-title" style="border-top: 1px solid #dddddd">
+          <span>回复({{retrial.length}})</span>
+        </div>
+        <div v-if="retrial.length > 0">
+            <template v-for="item in retrial">
               <div id="select-answer" class="clearfix">
-                <div class="options_man"><span></span><img class="man" src="../../src/assets/user-plus.png" alt=""></div>
-                <span class="answer_info">{{answer_info}}</span>
-                <span class="answer_time">{{answer_time}}</span>
+                <div class="options_man"><span>{{item.name}}</span><img class="man" src="../../src/assets/user-plus.png" alt=""></div>
+                <span class="answer_info">{{item.status}}</span>
               </div>
             </template>
         </div>
@@ -71,37 +68,40 @@
 </template>
 
 <script>
-  import { XButton , Flexbox, FlexboxItem,Cell ,Group} from 'vux'
+  import { XButton , Flexbox, FlexboxItem,Cell ,Group,AjaxPlugin } from 'vux'
   export default {
     name: 'hello',
     data () {
       return {
-        user:'张三',
-        passTotalNo:0,
-        relateTotalNo:2,
-        answerTotalNo:3,
-        reply_time:'1天',
-        reply_type:'事假',
-        begin_time:'2017-12-02',
-        over_time:'2017-12-02',
-        reason:'XXX',
-        minuteListValue:'2017-12-02',
-        minuteListValue2:'2017-12-02',
-        type:'事假',
-        answerList:[
-          {name:'王五'},
-          {name:'赵四'},
-          {name:'甜甜'}
-        ],
-        answer_info:'同意',
-        answer_time:'2017-12-02'
+        userData:{},
+        approver:[],
+        relevant:[],
+        retrial:[],
       }
     },
-    components: {XButton ,Flexbox, FlexboxItem ,Cell ,Group},
+    mounted:function () {
+        this.getAttendanceInfo()
+    },
+    components: {XButton ,Flexbox, FlexboxItem ,Cell ,Group,AjaxPlugin },
     computed: {
     },
-    methods: {}
+    methods: {
+      getAttendanceInfo (){
+        let dataStr={"id":1};
+        AjaxPlugin.$http.post('http://www.performance.com/wechat/getAttendanceInfo', dataStr)
+          .then((response) => {
+//            if (cb) cb(response.data)
+//            console.log(response.data)
+            this.userData=response.data.info;
+            this.approver=response.data.approver;
+            this.relevant=response.data.relevant;
+            this.retrial=response.data.retrial;
+          })
+      }
+    },
+
   }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
