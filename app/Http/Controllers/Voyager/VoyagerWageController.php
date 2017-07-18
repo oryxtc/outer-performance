45ce9e7f5ee209d3882ed374818a2ed01838d873
oryxtc <oryxtc@gmail.com>
2017-07-18 11:26:57 +0800

    修复 测试5

--- app/Http/Controllers/Voyager/VoyagerWageController.php
+++ app/Http/Controllers/Voyager/VoyagerWageController.php
@@ -583,7 +583,7 @@ class VoyagerWageController extends VoyagerBreadController
     {
         $today_date = date('Y-m', time());
         $min_limit_date = date('Y-m-d 00:00:00', strtotime(date('Y-m-01', strtotime($today_date)) . ' -1 month'));
-        $max_limit_date = date('Y-m-d 23:59:59', strtotime(date('Y-m-d', strtotime($min_limit_date)) . ' +1 month -1 day'));
+        $max_limit_date = date('Y-m-d 24:00:00', strtotime(date('Y-m-d', strtotime($min_limit_date)) . ' +1 month -1 day'));
         return ['min_limit_date' => $min_limit_date, 'max_limit_date' => $max_limit_date];
     }
 
--- app/Http/Controllers/WagesController.php
+++ app/Http/Controllers/WagesController.php
@@ -18,6 +18,8 @@ class WagesController extends Controller
     public $overtime_formal     = 0; //正式加班天数
     public $sick_probation      = 0; //试用病假天数
     public $sick_formal         = 0; //正式病假天数
+    public $think_probation     = 0; //试用事假天数
+    public $think_formal        = 0; //正式事假天数
     public $sick                = 0;//病假天数
     public $maternity_probation = 0; //试用产假天数
     public $maternity_formal    = 0; //正式产假天数
@@ -39,6 +41,9 @@ class WagesController extends Controller
 
         $this->maternity_probation = $this->getMaternityProbation();
         $this->maternity_formal = $this->getMaternityFormal();
+
+        $this->think_probation = $this->getThinkProbation();
+        $this->think_formal = $this->getThinkFormal();
 
         $this->provident_info = $this->getProvidentInfo();
 
@@ -539,6 +544,100 @@ class WagesController extends Controller
 
 
     /**
+     * 获取试用事假天数
+     * @return array
+     */
+    public function getThinkProbation()
+    {
+        $user = $this->user;
+        $job_number = $this->job_number;
+        $limit_date = $this->limit_date;
+        $formal_at = $user['formal_at'];
+        $total_day = 0;
+
+        $attendances_list = Attendance::where('job_number', $job_number)
+            ->where('status', Attendance::STATUS_PASSED)
+            ->where('type', '=', '事假')
+            ->whereDate('start_at', '<', $limit_date['max_limit_date'])
+            ->whereDate('end_at', '>', $limit_date['min_limit_date'])
+            ->get()
+            ->toArray();
+        foreach ($attendances_list as $item) {
+            //确认开始时间
+            if (strtotime($item['start_at']) < strtotime($limit_date['min_limit_date'])) {
+                $start_at = $limit_date['min_limit_date'];
+            } else {
+                $start_at = $item['start_at'];
+            }
+            //确认截止时间
+            if (strtotime($item['end_at']) > strtotime($limit_date['max_limit_date'])) {
+                $end_at = $limit_date['max_limit_date'];
+            } else {
+                $end_at = $item['end_at'];
+            }
+            //更具转正日期计算
+            if (strtotime($formal_at) < strtotime($start_at)) {
+                continue;
+            } elseif (strtotime($formal_at) <= strtotime($end_at) && strtotime($formal_at) >= strtotime($start_at)) {
+                $end_at = $formal_at;
+            }
+            //总时长(天数)
+            $total_at = (strtotime($end_at) - strtotime($start_at)) / 3600 / 24;
+            //增加对应试用加班天数
+            $total_day += $total_at;
+        }
+        return $total_day;
+    }
+
+
+
+    /**
+     * 获取正式事假天数
+     * @return array
+     */
+    public function getThinkFormal()
+    {
+        $user = $this->user;
+        $job_number = $this->job_number;
+        $limit_date = $this->limit_date;
+        $formal_at = $user['formal_at'];
+        $total_day = 0;
+        $attendances_list = Attendance::where('job_number', $job_number)
+            ->where('status', Attendance::STATUS_PASSED)
+            ->where('type', '=', '事假')
+            ->whereDate('start_at', '<', $limit_date['max_limit_date'])
+            ->whereDate('end_at', '>', $limit_date['min_limit_date'])
+            ->get()
+            ->toArray();
+
+        foreach ($attendances_list as $item) {
+            //确认开始时间
+            if (strtotime($item['start_at']) < strtotime($limit_date['min_limit_date'])) {
+                $start_at = $limit_date['min_limit_date'];
+            } else {
+                $start_at = $item['start_at'];
+            }
+            //确认截止时间
+            if (strtotime($item['end_at']) > strtotime($limit_date['max_limit_date'])) {
+                $end_at = $limit_date['max_limit_date'];
+            } else {
+                $end_at = $item['end_at'];
+            }
+            //更具转正日期计算
+            if (strtotime($formal_at) > strtotime($end_at)) {
+                continue;
+            } elseif (strtotime($formal_at) <= strtotime($end_at) && strtotime($formal_at) >= strtotime($start_at)) {
+                $start_at = $formal_at;
+            }
+            //总时长(天数)
+            $total_at = (strtotime($end_at) - strtotime($start_at)) / 3600 / 24;
+            //增加对应试用加班天数
+            $total_day += $total_at;
+        }
+        return $total_day;
+    }
+
+    /**
      * 获取病假天数
      * @return float
      */
@@ -598,7 +697,7 @@ class WagesController extends Controller
         }
         //试用期总时长(天数)
         $probation_total_at = (strtotime($probation_end_at) - strtotime($probation_start_at)) / 3600 / 24;
-        $probation_total_at = floor($probation_total_at - $this->sick_probation - $this->maternity_probation);
+        $probation_total_at = floor($probation_total_at - $this->sick_probation - $this->maternity_probation - $this->think_probation);
         $this->probation = $probation_total_at;
         return $probation_total_at;
     }
@@ -636,7 +735,7 @@ class WagesController extends Controller
         }
         //正式期期总时长(天数)
         $formal_total_at = (strtotime($formal_end_at) - strtotime($formal_start_at)) / 3600 / 24;
-        $formal_total_at = ceil($formal_total_at - $this->sick_formal - $this->maternity_formal);
+        $formal_total_at = ceil($formal_total_at - $this->sick_formal - $this->maternity_formal - $this->think_formal);
         $this->formal = $formal_total_at;
         return $formal_total_at;
     }
@@ -731,7 +830,7 @@ class WagesController extends Controller
     {
         $today_date = date('Y-m', time());
         $min_limit_date = date('Y-m-d 00:00:00', strtotime(date('Y-m-01', strtotime($today_date)) . ' -1 month'));
-        $max_limit_date = date('Y-m-d 23:59:59', strtotime(date('Y-m-d', strtotime($min_limit_date)) . ' +1 month -1 day'));
+        $max_limit_date = date('Y-m-d 24:00:00', strtotime(date('Y-m-d', strtotime($min_limit_date)) . ' +1 month -1 day'));
         return ['min_limit_date' => $min_limit_date, 'max_limit_date' => $max_limit_date];
     }
 
