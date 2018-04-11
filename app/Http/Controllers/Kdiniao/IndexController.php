d3e95651e7e43efa33bf8b6d52c0f0966698d4e1
oryxtc <oryxtc@gmail.com>
2018-04-11 16:22:13 +0800

    test kdiniao

--- app/Http/Controllers/Kdiniao/IndexController.php
+++ app/Http/Controllers/Kdiniao/IndexController.php
@@ -94,7 +94,7 @@ class IndexController extends Controller
         echo "电子面单接口提交内容：<br/>".$jsonParam;
         $jsonResult = $this->submitEOrder($jsonParam);
         echo "<br/><br/>电子面单提交结果:<br/>";
-        print_r($jsonResult['PrintTemplate']) ;
+        echo htmlspecialchars_decode($jsonResult['PrintTemplate']);
 
         //解析电子面单返回结果
         $result = json_decode($jsonResult, true);
